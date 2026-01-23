2e00b215219c2015cac4f7ec621d9ac2f8f6ea24
Brouz557 <136919055+Brouz557@users.noreply.github.com>
2026-01-22 22:45:56 -0600

    Fix assignment of line_test_val in session state

--- hockey_prop_stop_app - test.py
+++ hockey_prop_stop_app - test.py
@@ -349,7 +349,8 @@ with col_line:
 if "line_test_val" not in st.session_state:
     st.session_state.line_test_val = line_test
 elif st.session_state.line_test_val != line_test:
-    st.session_state.line_test_val =   
+    st.session_state.line_test_val =  line_test 
+    
         html_table=df[existing_cols].to_html(index=False,escape=False)
 
     components.html(f"""
